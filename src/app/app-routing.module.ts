767bbb0942539ffdefeb2df20c1a153f3bda6162
Polina Berest <pamapolya@gmail.com>
2024-11-21 23:14:30 +0200

    File structure донє

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }, // Маршрут для компонента
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Перенаправлення за замовчуванням
+  { path: '**', redirectTo: '/home' } // Перенаправлення для невідомих маршрутів
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
